9649e76047da68a4854705450a58331980166080
cvetko1996 <cvetanik996@gmail.com>
2018-05-14 17:18:20 +0200

    skelet

--- ChatApp.py
+++ ChatApp.py
@@ -1,19 +1,31 @@
 from flask import Flask, request, jsonify
+import pymysql
+
+nesto = 'export LC_ALL=C'
+
+
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-@app.route("/data", methods=['POST'])
-def send():
+@app.route('/login',methods=['POST'])
+def login():
     data = request.get_json()
-    s = data['name']
-    x = {'info':'yeaa', 'name':s}
-    return jsonify(x)
+    if 'username' and 'password' in data.keys():
+        return jsonify({'info': 'succes'})
+    else:
+        return jsonify({'info': 'fail'})
 
+@app.route('/register',methods=['POST'])
+def register():
+    data = request.get_json()
+    if 'username' and 'password' and 'fname' and 'lname' and 'email' in data.keys():
+        return jsonify({'info':'succes!'})
+    else:
+        return jsonify({'info': 'fail'})
 
 if __name__ == '__main__':
-    app.run(port=6000)
+    app.run(debug=True)
